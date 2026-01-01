962a2ae18619349f7fbf6bc1323a57320b365eec
Ryan Juma <ryanjjuma15@gmail.com>
2026-01-01 14:15:30 -0500

    Added Basic structure of the program. Includes a function that will print the menu, a loop that will ask the user if they want to continue, and an outline of the switch case conditions that will be used later to call on functions from different classes.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,55 @@
 #include "Student.h"
 #include "StudentManager.h"
 
+void printMenu();
+
 int main() {
 
+    char choice;
+    int selection;
+
+    do {
+
+    printMenu();
+
+    std::cout << "Enter Mode: \n";
+    std::cin >> selection;
 
 
+    switch (selection) {
+        case 1:
+            //Will call on an add student function
+            break;
+        case 2:
+            //will call on a view student function
+            break;
+        case 3:
+            //will call on a save to file function
+            break;
+        case 4:
+            //will call on a load to file function
+            break;
+        case 5:
+            //will break out of the menu
+            break;
+        default:
+            std::cout << "Invalid Selection!\n";
+    }
 
+    std::cout << "Do you want to run the program again. (y/Y).\n";
+    std::cin >> choice;
 
+    } while (choice == 'y'  || choice == 'Y');
 
+    std::cout << "Thanks for Using the Program.\n";
     return 0;
+}
+
+void printMenu() {
+    std::cout << "==== Student Manager ====\n";
+    std::cout << "1. Add Student\n";
+    std::cout << "2. View Students\n";
+    std::cout << "3. Save to file\n";
+    std::cout << "4. Load from file\n";
+    std::cout << "5.exit\n";
 }
