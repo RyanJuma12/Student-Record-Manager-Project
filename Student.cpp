44501ca4e6f4bbde3826fdaef5d5da8db7870e3b
Ryan Juma <ryanjjuma15@gmail.com>
2026-01-01 18:26:27 -0500

    Described the functions for the Student.cpp file.

--- Student.cpp
+++ Student.cpp
@@ -1,5 +1,25 @@
 //
-// Created by thoma on 12/31/2025.
+// Created by Ryan J on 12/31/2025.
 //
 
+#include <iostream>
+#include<string>
 #include "Student.h"
+#include "StudentManager.h"
+
+Student::Student(int x, std::string y, double z)
+    :ID(x), name(y), GPA(z)
+{
+}
+
+int Student::getID() const {
+    return ID;
+}
+
+std::string Student::getName() const {
+    return name;
+}
+
+double Student::getGPA() const {
+    return GPA;
+}
