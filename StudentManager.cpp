bbb3ec50fd1c4c4afd3d793cdc0fef55d0f39409
Ryan Juma <ryanjjuma15@gmail.com>
2026-01-01 18:54:30 -0500

    Configured functions to add students and to display students.

--- StudentManager.cpp
+++ StudentManager.cpp
@@ -1,5 +1,40 @@
 //
-// Created by thoma on 12/31/2025.
+// Created by Ryan J on 12/31/2025.
 //
 
+#include <iostream>
+#include<string>
+#include<fstream>
+#include "Student.h"
 #include "StudentManager.h"
+
+StudentManager::StudentManager()
+{
+}
+
+void StudentManager::addStudent(int ID, std::string name, double GPA) {
+    Student newStudentobj(ID, name, GPA);
+    students.push_back(newStudentobj);
+}
+
+void StudentManager::displayStudents() const {
+    if (students.empty()) {
+        std::cout << "There are No Students to Display.\n";
+        return;
+    }
+    for (size_t i = 0; i < students.size(); ++i) {
+        const Student& student = students[i];
+        std::cout << "Student " << (i + 1) << ":\n";
+        std::cout << "ID: " << student.getID() << "\n";
+        std::cout << "Name: " << student.getName() << "\n";
+        std::cout << "GPA: " << student.getGPA() << "\n\n";
+    }
+}
+
+void StudentManager::saveStudents(const std::string &filename) {
+
+}
+
+void StudentManager::loadStudents(const std::string &filename) {
+
+}
