a639caedb1dedf6ca63d86a7f86b58316be4ea0e
Ryan Juma <ryanjjuma15@gmail.com>
2026-01-01 18:06:52 -0500

    Added parameters and described class functions for Student.h header file.

--- Student.h
+++ Student.h
@@ -1,13 +1,25 @@
 //
-// Created by thoma on 12/31/2025.
+// Created by Ryan J on 12/31/2025.
 //
 
 #ifndef STUDENT_RECORD_MANAGER_PROJECT_STUDENT_H
 #define STUDENT_RECORD_MANAGER_PROJECT_STUDENT_H
+#include<string>
 
 
 class Student {
+    public:
+        Student(int x, std::string y, double z);
+        int getID() const;
+        std::string getName() const;
+        double getGPA() const;
+    private:
+        int ID;
+        std::string name;
+        double GPA;
+
 };
+
 
 
 #endif //STUDENT_RECORD_MANAGER_PROJECT_STUDENT_H
