a33c4f51a090a0ab81a43137e6dea17ec58d46b9
Ryan Juma <ryanjjuma15@gmail.com>
2026-01-01 18:07:41 -0500

    Added parameters and described class functions for StudentManager.h header file.

--- StudentManager.h
+++ StudentManager.h
@@ -4,9 +4,19 @@
 
 #ifndef STUDENT_RECORD_MANAGER_PROJECT_STUDENTMANAGER_H
 #define STUDENT_RECORD_MANAGER_PROJECT_STUDENTMANAGER_H
+#include "Student.h"
+#include <vector>
 
 
 class StudentManager {
+    public:
+        StudentManager();
+        void addStudent(int ID, std::string name, double GPA);
+        void displayStudents() const;
+        void saveStudents(const std::string& filename);
+        void loadStudents(const std::string& filename);
+    private:
+        std::vector<Student> students;
 };
 
 
